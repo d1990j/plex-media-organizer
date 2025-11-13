0760c5e7df6202d7303c01f6ab0d31479f36c41b
Dewain Jones <dewainjones@icloud.com>
2025-11-12 22:09:57 -0600

    Add TODO message for plans and add test files to gitignore

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from logic import *
 from ui import *
 import tkinter as tk
 
+# TODO
+# Reconfiure the files to run as instances. The UI will be an instance that simply builds the UI.
+# For functionality of buttons, make a file for button logic that has an instane connected to the UI
+# so that the appropriate calls can be made back and forth for changes.
+# Make proper get/set functions in each, as needed, to allow safe access and changes.
+
 def main():
 
     # set up UI
